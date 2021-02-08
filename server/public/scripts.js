3e60ff489f36102f1f9803781a2da3ea4166a052
Mackenzie Salloum <mackenziesalloum@gmail.com>
2021-02-08 08:22:29 -0800

    Display latest 50 songs in chart

--- server/public/scripts.js
+++ server/public/scripts.js
@@ -13,45 +13,24 @@ function getHashParams() {
   return hashParams;
 }
 
-function createChart() {
+function createChart(data, label, labels) {
   const ctx = document.getElementById("chart").getContext("2d");
   const chart = new Chart(ctx, {
-    type: "bar",
+    type: "line",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: labels,
       datasets: [
         {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
+          label: label,
+          data: data,
         },
       ],
     },
     options: {
       scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
+        x: {
+          type: "timeseries",
+        },
       },
     },
   });
@@ -81,7 +60,7 @@ function updateToken() {
 }
 
 function grabData() {
-  fetch("https://api.spotify.com/v1/me", {
+  fetch("https://api.spotify.com/v1/me/tracks?limit=50", {
     method: "GET",
     headers: {
       Authorization: "Bearer " + accessToken,
@@ -93,13 +72,28 @@ function grabData() {
       }
     })
     .then((data) => {
-      document.querySelector("#data-placeholder").innerHTML = JSON.stringify(
-        data
-      );
+      dates = data.items.map((song) => song.added_at);
+      const freqMap = new Map();
+
+      dates.forEach(function (time) {
+        const date = new Date(time);
+        const monthName = date.toLocaleString("en-us", {
+          month: "short",
+        });
+
+        const key = monthName + "-" + date.getFullYear();
+        let count = freqMap.get(key) || 0;
+        freqMap.set(key, ++count);
+      });
+
+      document.querySelector("#data-placeholder").innerHTML = dates.join("\n");
+      createChart([...freqMap.values()], "Save Date", [...freqMap.keys()]);
     })
     .catch(console.log);
 }
 
 document.querySelector("#get-data").addEventListener("click", grabData);
 
-createChart();
+if (accessToken) {
+  document.querySelector("#login-button").remove();
+}
